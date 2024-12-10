85a771100b1751dbcc66b030cece4005177171cb
oussamalkd <oussamalkd99@gmail.com>
2024-12-10 14:10:05 +0100

    feat: total sales

--- src/components/Analytics/TotalSales.vue
+++ src/components/Analytics/TotalSales.vue
@@ -1,9 +1,9 @@
 <script setup></script>
 
 <template>
-  <div class="total-amount border border-secondary rounded-xl">
-    <span> net income</span>
-    <span class="total-amount"> $193.000,44 </span>
+  <div class="total-amount border border-secondary rounded-xl w-full flex flex-col p-2">
+    <span> net income: </span>
+    <span class="total-amount text-center text-4xl my-5 font-semibold"> $193.000,44 </span>
   </div>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup>
+import TotalSales from '@/components/Analytics/TotalSales.vue'
 import TrendingList from '@/components/Analytics/Trending/List.vue'
 </script>
 
 <template>
   <h1 class="text-3xl font-bold">Dashboard</h1>
+  <div class="sales-amount max-w-xs">
+    <TotalSales />
+  </div>
   <div class="trnding-products-container">
     <TrendingList />
   </div>
